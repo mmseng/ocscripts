8673c7610d03a5d4e7939376b840ea5ee1bd71d4
mmseng <mmseng@users.noreply.github.com>
2023-03-19 22:41:45 -0500

    Update dig.lua

--- dig.lua
+++ dig.lua
@@ -148,7 +148,7 @@ local maxHeight = 256 -- Max height of world, for use with options that dig maxi
 --===========
 
 -- Log
-local function log(msg, level)
+local function log(msg,level)
 	level = level or 0
 	indent = ""
 	for(i = 0; i < level; i += 1) {
