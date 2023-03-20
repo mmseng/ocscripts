39c777dc510b8cd9268ee6a5f1d7845cef18f4e4
mmseng <mmseng@users.noreply.github.com>
2023-03-19 23:46:37 -0500

    Create cl.lua

---
+++ cl.lua
@@ -0,0 +1,28 @@
+function log(msg)
+	io.write(msg)
+end
+
+local c = require("component")
+local cl = c.chunkloader
+
+local state = cl.isActive()
+local newState
+if state then
+	log("Chunkloader is currently active. Setting to inactive...")
+	cl.setActive(false)
+	newState = cl.isActive()
+	if newState then
+		log("Failed to set chunkloader to inactive!")
+	else
+		log("Successfully set chunkloader to inactive.")
+	end
+else
+	log("Chunkloader is current inactive. Setting to active..."
+	cl.setActive(true)
+	newState = cl.isActive()
+	if newState then
+		log("Successfully set chunkloader to active.")
+	else
+		log("Failed to set chunkloader to active!")
+	end
+end
