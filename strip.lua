0c027ee04d3df6271f4a5314677de70d4a856b34
mmseng <mmseng@users.noreply.github.com>
2023-04-18 22:16:56 -0500

    Update strip.lua

--- strip.lua
+++ strip.lua
@@ -1,6 +1,11 @@
 local component = require("component")
 local robot = require("robot")
+local sides = require("sides")
 local r = component.robot
+
+local function log(msg)
+	io.write(msg)
+end
 
 function needDrop()
 	
@@ -20,6 +25,7 @@ function needDrop()
 end
 
 local function dropAll()
+	log("    Dropping...")
 	for slot = 1, 16 do
 		if robot.count(slot) > 0 then
 			robot.select(slot)
@@ -32,12 +38,22 @@ local function dropAll()
 			until robot.count(slot) == 0
 		end
 	end
+	log("        Done dropping...")
 end
 
+log("Starting loop...")
 while(true) do
-	r.suck(3)
+	log("\nStarting loop iteration...")
+	log("    Sucking...")
+	r.suck(sides.forward)
+	log("        Done sucking.")
 	
+	log("    Checking if full...")
 	if(needDrop()) then
+	    log("        Full.")
 		dropAll()
+	else
+		log("        Not full.")
 	end
+	log("Done with loop iteration.")
 end
